10fda5426fac8e5c5778edfdcee7934291ad24cf
leeksh06 <104353105+leeksh06@users.noreply.github.com>
2025-07-31 22:19:59 +0900

    Delete camping directory

--- camping/android/app/src/main/kotlin/com/example/camping/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.camping
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- camping/android/app/src/main/kotlin/kr/ac/kumoh/camping/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package kr.ac.kumoh.camping
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
